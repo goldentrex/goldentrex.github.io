e15629ea96a0f98d95eabc71bb908469d016f34a
VictorGAYA <g.victor@hotmail.fr>
2026-03-12 11:59:49 +0800

    New experiences

---
+++ header.js
@@ -0,0 +1,7 @@
+document.getElementById('site-header').innerHTML = `
+  <header>
+    <img src="final_picture.JPEG" alt="Victor Gaya Photo">
+    <h1>Victor Gaya</h1>
+    <p>PhD – Artificial Intelligence for remote sensing</p>
+  </header>
+`;
